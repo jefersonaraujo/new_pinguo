b89bee31121e986eb84200d33c830b92d6c38e50
Jeferson Araujo <jefersonaraujo95@gmail.com>
2016-04-25 19:40:45 -0300

    commit

--- game.bkp16.lua
+++ game.bkp16.lua
@@ -9,18 +9,17 @@ local onMovePinguo = {}
 local addCenario = {}
 local addInicialBlocos = {}
 local addPinguo = {}
-local addtime = {}
+local addLive = {}
 local addInicialBlocos2 = {}
 local addBloco = {}
 local blocos 
 local sheet1
 local pinguo 
-local timeTF
 local scoreTF 
-local score = 0
-local time --imagem obj
-local timesTF = 3
-local times = 70 --- temo
+local score = 0 
+local live
+local livesTF = 3
+local lives
 local alertScore 
 local blocoDOWN = 0 
 local bola
@@ -29,31 +28,21 @@ local bloco
 local Ax = 0
 local Ay = 0
 local blocos = display.newGroup()
-_W = display.contentWidth; -- Get the width of the screen
-_H = display.contentHeight; -- Get the height of the screen
-motionx = 0; -- Variable used to move character along x axis
-speed = 4; -- Set Walking Speed
-playerInAir = false; -- Set a boolean of whether our guy is in the air or not
-somDeImpacto = audio.loadSound("music/jump.wav")
-local largura = display.contentWidth;
-local altura = display.contentHeight;
-local bgHeight = screenH
-local bgWidth = -600
-local up
 
+somDeImpacto = audio.loadSound("music/jump.wav")
 --somDeGameOver = audio.loadSound("sounds/destructe.mp3")
 ---som = audio.loadSound("sounds/3HAPPYSTAGE1.mp3")
-
 physics.setGravity( 0,9.8 )
-physics.setDrawMode("normal")
--- "scene:create()"
 
+ physics.setDrawMode("normal")
+-- "scene:create()"
 function scene:create( event )
    
 local physics = require("physics")
+---local show = require("show")
     physics.start()
-    
-    
+    --physics.setGravity(0,40,5)
+    --- physics.setGravity(0,30)
 
     local sceneGroup = self.view  
 --Runtime:addEventListener("enterFrame",show.make)
@@ -68,7 +57,10 @@ local physics = require("physics")
     --  background.height=screenH   * 2
      -- sceneGroup:insert(background)
        -- >> Criação do  Background<< --
-
+local largura = display.contentWidth;
+local altura = display.contentHeight;
+local bgHeight = screenH
+local bgWidth = -600
 
 local background = display.newImageRect('Imagens/bgplay4.png', largura, bgHeight)
 background.anchorX = 0
@@ -147,12 +139,8 @@ Runtime:addEventListener("touch", toque)
 
       scoreTF = display.newText('0',303,22,system.nativeFont,30)
       scoreTF:setTextColor(0,0,0)
-        
-
-      timeTF = display.newText('0',150,22,system.nativeFont,30)
-      timeTF:setTextColor(0,0,0) 
-
-
+      livesTF = display.newText('x3',289,56,system.nativeFont,30)
+      livesTF:setTextColor(0,0,0)
       local chao = display.newImage("img/chao.png")
       chao.y = screenW  * 1.8
       chao.x = 370
@@ -161,11 +149,6 @@ Runtime:addEventListener("touch", toque)
       sceneGroup:insert(chao)
      -- addInicialBlocos2('add')
 
-
--------- chao colicao 
- local colisaobloco = display.newRect(300,1400,display.contentWidth,3)
- colisaobloco:setFillColor( 0.5 )
- colisaobloco.name="removebloco"
      
       -->criar paredes
 
@@ -204,7 +187,38 @@ Runtime:addEventListener("touch", toque)
     end
     
  ---funcao temporaria para blocos nao dinamicos   
+function addInicialBlocos2(action)
+    if(action == 'add') then
+        local bloco = display.newImage("img/bloco.png")
+        bloco.height=100
+        bloco.width = 150
+        bloco.x = 70
+        bloco.y = 1200 - blocoDOWN
+        physics.addBody(bloco, "static", {density=1, friction=0.3, bounce=0.3})
+        sceneGroup:insert(bloco)
 
+        local bloco2 = display.newImage("img/bloco.png")
+        bloco2.height=100
+        bloco2.width = 150
+        bloco2.x = 250
+        bloco2.y = 900 - blocoDOWN
+        physics.addBody(bloco2, "static", {density=1, friction=0.3, bounce=0.3})
+        sceneGroup:insert(bloco2)
+
+
+        local bloco3 = display.newImage("img/bloco.png")
+        bloco3.height=100
+        bloco3.width = 150
+        bloco3.x = 700
+        bloco3.y = 1000 - blocoDOWN
+        physics.addBody(bloco3, "static", {density=1, friction=0.3, bounce=0.3})
+        sceneGroup:insert(bloco3)
+
+    else 
+       -- depois removeremoveEventListener(
+    end
+end
+ 
 
 
 function addInicialBlocos(n)
@@ -218,49 +232,60 @@ function addInicialBlocos(n)
         bloco.width = 150
         bloco.bodyType = 'static'
         sceneGroup:insert(bloco)
-       --- blocos:insert(bloco)
+        blocos:insert(bloco)
     end
     addPinguo()
 end    
 -- funcao add pinguo 
 function addPinguo()
 -->pinguo
+    -- pinguo = display.newImage("img/pinguo.png")
+    -- pinguo.width=80
+    -- pinguo.height=80
+    -- pinguo.x = screenW * 0.5
+    -- pinguo.y = screenH /2 
+    -- pinguo.isFixedRotation = true
+    -- physics.addBody( pinguo,"dynamic", {density=1, friction=0.3, bounce=0.3})
 
+    -- Create ball (pinguoacter)
 --pinguo = display.newImageRect( worldGroup, "img/pp.png", 100, 90 )
 ---sheet1 = graphics.newImageSheet( "img/pp1.png", { width=180, height=85, numFrames=3})
-   pinguo = display.newImage( "img/pp1.png" )
-   pinguo.height = 90
-   pinguo.width = 90
-
-   physics.addBody( pinguo,  {  bounce=0.2, friction=0.3} )
-   pinguo.x = 600 ; pinguo.y = 200
-   pinguo.isFixedRotation = true
-   pinguo.alpha = 0.7
-   pinguo:setLinearVelocity( 120,0 )
-   sceneGroup:insert(pinguo)
-   gameFuncoes('add')
+ pinguo = display.newImage( "img/pp1.png" )
+ pinguo.height = 90
+ pinguo.width = 90
 
 
+physics.addBody( pinguo,  {  bounce=0.3, friction=0.3} )
+ pinguo.x = 600 ; pinguo.y = 200
+--pinguo.rotation = 360
+
+--pinguo.rotation = 180
+pinguo.isFixedRotation = true
+pinguo.alpha = 0.7
+pinguo:setLinearVelocity( 120,0 )
+---pinguo.angularVelocity = 160
+    sceneGroup:insert(pinguo)
+    gameFuncoes('add')
 end
     
     
-
+_W = display.contentWidth; -- Get the width of the screen
+_H = display.contentHeight; -- Get the height of the screen
+motionx = 0; -- Variable used to move character along x axis
+speed = 4; -- Set Walking Speed
+playerInAir = false; -- Set a boolean of whether our guy is in the air or not
 ----------------------------------------------------------------------------------------
---BOTOES
 -- Add left joystick button
 local left = display.newImage ("imagens/btn_arrow.png")
----sceneGroup:insert(left)
   left.x = 45; left.y = 280;
   left.rotation = 180;
 
 -- Add right joystick button
 local right = display.newImage ("imagens/btn_arrow.png")
---sceneGroup:insert(right)
   right.x = 120; right.y = 282;
 
 -- Add Jump button
 local up = display.newImage ("imagens/btn_arrow.png")
---sceneGroup:insert(up)
   up.x = 440; up.y = 280;
   up.rotation = 270;
 
@@ -312,25 +337,15 @@ up:addEventListener("touch",up)
 
 -- Detect whether the player is in the air or not
 function onCollision( event )
-  if(event.object1.myName == "blocos" and event.object2.myName == "pinguo") then
-    print("onCollision")
-    DESCER()
+  if(event.object1.myName == "grass" and event.object2.myName == "pinguo") then
+    playerInAir = false;
   end
 end
 Runtime:addEventListener( "collision", onCollision )
  
 
      
- function onTime()
-  times = times  - 00.1
-  timeTF.text = times
-end  
-
-function onScore()
-  score = score  + 10
-  scoreTF.text = score
-end  
-
+   
 
 --------------------------<fucoesPinguo>------------------------------------
 -- Create blocos
@@ -338,24 +353,23 @@ local function createbloco( x, y )
 
   bloco = display.newImageRect( worldGroup, "img/bloco.png", 150, 90 )
   physics.addBody( bloco, "static", { bounce=0.3, friction=0.3 } )
-  bloco.collType = "passar"
+  bloco.collType = "passthrough"
   bloco.x, bloco.y = x, y
-  bloco.name = "blocos"
+
   blocos:insert(bloco)
-  --sceneGroup:insert(blocos)
+  ---sceneGroup:insert(background)
   bloco:toBack()
 end
 
 
 
-createbloco( 60, 840)
-createbloco( 260, 940)
-createbloco( 500, 920 )
-createbloco( 170, 1120)
-createbloco( 390, 120)
+createbloco( 40, 820)
+createbloco( 260, 920)
+createbloco( 480, 900 )
+createbloco( 150, 1100)
+createbloco( 370, 1200)
 
-
- local function DESCER()
+ function DESCER(event)
 --  Ax = Ax + 20
 --  Ay = Ay + 100
 --  print (Ay)
@@ -367,34 +381,34 @@ createbloco( 390, 120)
 
 
 
- 
+ if ( "passthrough" == event.other.collType ) then
     
       print( "COLISAO BLOCO" )
-  -- for i = 0, n do
-  --      --- blocos:insert(bloco)
-  --   end    
-  blocos[blocos.numChildren].y = blocos[blocos.numChildren - 1].y + 0.2
-  blocos[blocos.numChildren - 1].y = blocos[blocos.numChildren - 1].y + 0.2
-  blocos[blocos.numChildren - 2].y = blocos[blocos.numChildren - 2].y + 0.2
-  blocos[blocos.numChildren - 3].y = blocos[blocos.numChildren - 3].y + 0.2
-  blocos[blocos.numChildren - 4].y = blocos[blocos.numChildren - 4].y + 0.2
+  -- blocos[blocos.numChildren - 1].y = blocos[blocos.numChildren - 1].y + 0.9
+  -- blocos[blocos.numChildren - 2].y = blocos[blocos.numChildren - 2].y + 0.9
+  -- blocos[blocos.numChildren - 3].y = blocos[blocos.numChildren - 3].y + 0.9
+  -- blocos[blocos.numChildren - 4].y = blocos[blocos.numChildren - 4].y + 0.9
+end
+
  
 end
 
--->pre colisao pinguo
+
 local function localPreCollision( self, event )
 
-  if ( "passar" == event.other.collType ) then
-      
-      
-      print("COLISAO BLOCO")
-     -- DESCER()
-    --  audio.play(somDeImpacto)
+  if ( "passthrough" == event.other.collType ) then
+  
+      print( "COLISAO BLOCO" )
+      audio.play(somDeImpacto)
       local teste2=blocos[blocos.numChildren - 1].y 
       teste2= teste2 + 0.9
-      print(teste2)
-  
-    
+  print(teste2)
+  blocos[blocos.numChildren - 2].y = blocos[blocos.numChildren - 2].y + 0.9
+  blocos[blocos.numChildren - 3].y = blocos[blocos.numChildren - 3].y + 0.9
+  blocos[blocos.numChildren - 4].y = blocos[blocos.numChildren - 4].y + 0.9
+    -- Compare Y position of pinguoacter "base" to bloco top
+    -- A slight increase (0.2) is added to account for collision location inconsistency
+    -- If collision position is greater than bloco top, void/disable the specific collision
     if ( self.y+(self.height*0.5) > event.other.y-(event.other.height*0.5)+0.2 ) then
 
       if event.contact then
@@ -420,147 +434,134 @@ local function onMovePinguo( event )
     end
 
 end
+-----------------add blocos ---------------------------
+function addBloco()
+    local r = math.floor(math.random()*4)
+    if(r ~= 0) then
+        local bloco = display.newImage('img/bloco.png')
+        bloco.x = math.random()*(display.contentWidth - (bloco.width * 1.5))
+        bloco.y =math.random()* display.contentHeight - bloco.height
+        --pinguo.x = screenW * 0.5
+       -- pinguo.y = screenH /2 
+       bloco.height=100
+        bloco.width = 150
+        physics.addBody(bloco ,"static",{density = 1, bounce = 0, friction="1"})
+       -- blocos:insert(bloco)
+        sceneGroup:insert(bloco)
+        blocos:insert(bloco)
+        sceneGroup:insert(blocos)
+         print("inserindo  bloco")
+    else
+        local badbloco = display.newImage('img/bloco.png')
+        badbloco.name = 'bad'
+        physics.addBody(badbloco,{density = 1 ,bounce = 0})
+        badbloco.bodyType = 'static'
+        badbloco.x = math.random() * (display.contentWidth - (badbloco.width * 0.5))
+        badbloco.y = display.contentHeight + badbloco.height
 
-
+       -- blocos:insert(badbloco)
+        
+        blocos:insert(badbloco)
+        sceneGroup:insert(badbloco)
+        sceneGroup:insert(blocos)
+        print("inserindo bad bloco")
+    end
+end
 
 system.setAccelerometerInterval(10)
 
 
 
-function addtime()
-    time = display.newImage('img/live.png')
-
-   time.name = 'time'
-    local blocoRandom=math.random(4) 
-    time.x = blocos[blocos.numChildren - blocoRandom].x 
-    time.y = blocos[blocos.numChildren - blocoRandom].y - time.height
-  physics.addBody(time,{density = 1,friction = 0,bounce = 0})
-   sceneGroup:insert(time)
+function addLive()
+    live = display.newImage('img/live.png')
+    live.name = 'live'
+    live.x = blocos[blocos.numChildren - 1].x
+    live.y = blocos[blocos.numChildren - 1].y - live.height
+  physics.addBody(live,{density = 1,friction = 0,bounce = 0})
+   sceneGroup:insert(live)
  print("time")
-
 end
-
-function addUp()
-    up = display.newImage('img/lo.png')
-    up.name = 'up'
-    local blocoRandom=math.random(4) 
-    up.x = blocos[blocos.numChildren - blocoRandom].x 
-    up.y = blocos[blocos.numChildren - blocoRandom].y - up.height
-  physics.addBody(up,{density = 1,friction = 0,bounce = 0})
-   sceneGroup:insert(up)
- print("up")
-
-end
-
 -----------------
  function gameFuncoes(action)
     if(action == 'add') then
          Runtime:addEventListener('accelerometer',onMovePinguo)
          pinguo.preCollision = localPreCollision
          pinguo:addEventListener( "preCollision", pinguo )
-         Runtime:addEventListener('enterFrame',onJump)
-         Runtime:addEventListener('enterFrame',DESCER)
+         Runtime:addEventListener('enterFrame',pulo)
         -- blockTimer =  timer.performWithDelay(200, DESCER,0)
-         timeTimer = timer.performWithDelay(8000,addtime,0)
-         upTimer = timer.performWithDelay(8000,addUp,0)
+         liveTimer = timer.performWithDelay(8000,addLive,0)
          pinguo:addEventListener('collision',collisionHandler)
     else
         print("else")
-        Runtime:removeEventListener('accelerometer',onMovePinguo)
-        Runtime:removeEventListener('enterFrame',onJump)
-       -- timer.cancel(blockTimer)
-     --   timer.cancel(timeTimer)
-       -- blockTimer = nil
-        timeTimer = nil
-        pinguo:removeEventListener('collision',collisionHandler)
+      Runtime:removeEventListener('accelerometer',onMovePinguo)
+      Runtime:removeEventListener('enterFrame',pulo)
+     -- timer.cancel(blockTimer)
+     -- timer.cancel(liveTimer)
+   -- blockTimer = nil
+    --  liveTimer = nil
+    --  pinguo:removeEventListener('collision',collisionHandler)
 
     end
 end
 
-function onJump(e)
+function pulo(e)
     --pinguo Movement
 
-    onTime()
+    
 
    pinguo.y = pinguo.y - 4
-
+--blocoDOWN = blocoDOWN -2 
+   ---addInicialBlocos2('add')
+ --  print( blocoDOWN )
+   ---print("COLISAO")
+ 
+  --- print( blocos[blocos.numChildren) 
    
+    score = score  + 1
+  scoreTF.text = score
+    --Score
+    
+    --Lose lives
+    -- if(pinguo.y > display.contentHeight or pinguo.y < -5) then
+    --     pinguo.x = bloco[bloco.numChildren - 1].x
+    --     pinguo.y = bloco[bloco.numChildren - 1].y - pinguo.height
+    --     lives = lives - 1
+    --     livesTF.text = 'x' .. lives
+    -- end
 
-   if(times < 0) then
-        showAlert()
-   end
+    --Check for game over
+   -- if(lives < 0) then
+       -- showAlert()
+ --   end
 
    
 end
 
 function collisionHandler(e)
-    -- --Grab times
-    if(e.other.name == 'time') then
+    -- --Grab Lives
+    -- if(e.other.name == 'live') then
         
-        display.remove(e.other)
-        e.other = nil
-        times = times + 100
-        --score = score + 10
-        
-    
-    elseif(e.other.name == 'up') then
-        
-        display.remove(e.other)
-        e.other = nil
-        onScore()  
-
-    
-
-
-        
-    end
-
-
-
-    -- if(e.other.name == 'blocos') then
-        
-    --    -- blocos[blocos.numChildren - 1].y = blocos[blocos.numChildren - 1].y + 0.9
-    --    -- blocos[blocos.numChildren - 2].y = blocos[blocos.numChildren - 2].y + 0.9
-    --    -- blocos[blocos.numChildren - 3].y = blocos[blocos.numChildren - 3].y + 0.9
-    --    -- blocos[blocos.numChildren - 4].y = blocos[blocos.numChildren - 4].y + 0.9
-       
-        
+    --     display.remove(e.other)
+    --     e.other = nil
+    --     lives = lives + 1
+    --     print(lives)
+    --     livesTF.text = 'x' .. lives
+    --     print(lives)
     -- end
-
-    --Score
-    
-    --Lose times
-    -- if(pinguo.y > display.contentHeight or pinguo.y < -5) then
-    --     pinguo.x = bloco[bloco.numChildren - 1].x
-    --     pinguo.y = bloco[bloco.numChildren - 1].y - pinguo.height
-    --     times = times - 1
-    --     timesTF.text = 'x' .. times
-    -- end
-
-    --Check for game over
-
-
-    --Bad bloco
+    -- --Bad bloco
     -- if(e.other.name == 'bad') then
-    --     -- times = times - 1
-    --     -- print(times)
-    --     -- timesTF.text = 'x' .. times
-
-    --     print("BAD BLOCO")
+    --     lives = lives - 1
+    --     print(lives)
+    --     livesTF.text = 'x' .. lives
     -- end
 end
 
 function  showAlert()
     gameFuncoes('rmv')
-    local alert = display.newImage('img/alertBg.png',600,490)
-    alertScore = display.newText(scoreTF.text .. '!',600,490,native.systemFontBold,30)
-    --timesTF = display.newText('x3',289,56,system.nativeFont,30)
-    alertScore:setTextColor(0,0,0)  
+    local alert = display.newImage('img/alertBg.png',70,190)
+    alertScore = display.newText(scoreTF.text .. '!',134,240,native.systemFontBold,30)
+    livesTF.text = ''
     sceneGroup:insert(alert)
-    sceneGroup:insert(alertScore)
-    
-
-
    
 end
 
@@ -568,7 +569,17 @@ end
 
 
 
+--local function onFrame()
+ --   frameUpdate = true
 
+--end
+--frequencia bateria respostas
+
+--Runtime:addEventListener ("enterFrame", onFrame);
+
+--------------------te
+
+---blocoTimer =  timer.performWithDelay(800,addBloco,0)
  startGame()   
 
 end
@@ -585,7 +596,7 @@ function scene:show( event )
         -- Called when the scene is still off screen (but is about to come on screen).
     elseif ( phase == "did" ) then
         -- Called when the scene is now on screen.
-        -- Insert code here to make the scene come atime.
+        -- Insert code here to make the scene come alive.
         -- Example: start timers, begin animation, play audio, etc.
     end
 end
